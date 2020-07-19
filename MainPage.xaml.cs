396328d352cbba9ba0439a7869f6edd78c2838d2
codegression <68420420+codegression@users.noreply.github.com>
2020-07-19 13:34:54 +1000

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Devices.Lights;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Morse_Code_Generator
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        MorseCode morsecode;
+        Lamp lamp;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            morsecode = new MorseCode();
+            morsecode.NewTimeSegment += Morsecode_NewTimeSegment;      
+
+
+        }
+
+     
+
+        private void Morsecode_NewTimeSegment(bool On, string dotdash)
+        {
+            
+                try
+                {
+                    if (On)
+                    {
+                        this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                       {
+                           MainGrid.Background = new SolidColorBrush(Windows.UI.Colors.White);
+                           btnStop.Background = new SolidColorBrush(Windows.UI.Colors.White);
+                           btnStop.Foreground = new SolidColorBrush(Windows.UI.Colors.Black);
+                       });
+
+                        if (lamp!=null)
+                        {
+                             lamp.IsEnabled = true;
+                        }
+                    }
+                    else
+                    {
+                        this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                       {
+                           MainGrid.Background = new SolidColorBrush(Windows.UI.Colors.Black);
+                           btnStop.Background = new SolidColorBrush(Windows.UI.Colors.Black);
+                           btnStop.Foreground = new SolidColorBrush(Windows.UI.Colors.White);
+                       });
+                        if (lamp != null)
+                        {
+                                 lamp.IsEnabled = false;
+                        }
+                }
+                }
+                catch { }
+           
+        }
+
+        private void btnGenerate_Click(object sender, RoutedEventArgs e)
+        {
+            if (btnGenerate.Content.ToString()=="Play")
+            {
+                Generate();
+            }
+            else
+            {
+                Player.Stop();
+                btnGenerate.Content = "Play";
+                btnSave.Visibility = Visibility.Visible;
+            }        
+        
+        }
+
+        private async void Generate()
+        {
+            morsecode.WPM = (int)WPMSlider.Value;
+            morsecode.IsLoop = IsLoop.IsOn;
+
+            if (SoundSignal.IsSelected)
+            {
+                Player.Stop();
+                btnGenerate.Content = "Stop";
+                btnSave.Visibility = Visibility.Collapsed;
+                Player.IsLooping = IsLoop.IsOn;
+                int[] durations = null;
+                bool[] levels = null;
+                morsecode.Synthesize(textbox1.Text, out durations, out levels);
+
+                StorageFolder tempfolder = ApplicationData.Current.TemporaryFolder;
+                int j = await Sound.SynthesizeAsync(durations, levels, tempfolder.Path.ToString() + "\\MORSECODE.wav");
+
+
+                Windows.Storage.StorageFile file = await tempfolder.GetFileAsync("MORSECODE.wav");
+                var stream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read);
+                Player.SetSource(stream, file.ContentType);
+            }
+            else
+            {
+                if (lamp == null)
+                {
+                    lamp = await Lamp.GetDefaultAsync();
+                }
+                Player.Stop();
+                ButtonsStackPanel.Visibility = Visibility.Collapsed;
+                textbox1.Visibility = Visibility.Collapsed;
+                ControlsStackPanel.Visibility = Visibility.Collapsed;
+                TitleTextBlock.Visibility = Visibility.Collapsed;
+                btnStop.Visibility = Visibility.Visible;
+                int i = await morsecode.PlayAsync(textbox1.Text);
+                CleanUP();
+            }
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            morsecode.Stop();
+            Player.Stop();
+            CleanUP();
+            if (lamp != null)
+            {
+                lamp.IsEnabled = false;
+            }
+        }
+
+        private void CleanUP()
+        {
+            ButtonsStackPanel.Visibility = Visibility.Visible;
+            textbox1.Visibility = Visibility.Visible;           
+            ControlsStackPanel.Visibility = Visibility.Visible;
+            TitleTextBlock.Visibility = Visibility.Visible;           
+            btnStop.Visibility = Visibility.Collapsed;
+            SolidColorBrush brush = Application.Current.Resources["ApplicationPageBackgroundThemeBrush"] as SolidColorBrush;
+            MainGrid.Background = brush;
+          
+        }
+
+        private void SignalType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (LightSignal.IsSelected && btnSave!=null)
+            {
+                btnSave.Visibility = Visibility.Collapsed;
+            }
+            else if (btnSave != null)
+            {
+                btnSave.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Player_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            btnGenerate.Content = "Play";
+            btnSave.Visibility = Visibility.Visible;
+        }
+
+        private async void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            FileSavePicker savePicker = new FileSavePicker();
+            savePicker.SuggestedStartLocation = PickerLocationId.MusicLibrary;      
+            savePicker.FileTypeChoices.Add("WAV file", new List<string>() { ".wav" });            
+
+            StorageFile destinationfile = await savePicker.PickSaveFileAsync();
+            if (destinationfile != null)
+            {
+                
+                int[] durations = null;
+                bool[] levels = null;
+                morsecode.Synthesize(textbox1.Text, out durations, out levels);
+
+                StorageFolder tempfolder = ApplicationData.Current.TemporaryFolder;
+                int j = await Sound.SynthesizeAsync(durations, levels, tempfolder.Path.ToString() + "\\MORSECODE.wav");
+
+
+                StorageFile originalfile = await tempfolder.GetFileAsync("MORSECODE.wav");
+                await originalfile.CopyAndReplaceAsync(destinationfile);
+            }
+          
+        }
+    }
+}
---
+++ Sound.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Morse_Code_Generator
+{
+    public class Sound
+    {
+        public static Task<int> SynthesizeAsync(int[] durations, bool[] level, string filename, int Frequency = 1100, int Amplitude = 500)
+        {
+            return Task.Run(
+               () => { return Synthesize(durations, level, filename, Frequency, Amplitude); }
+               );
+        }
+        public static int Synthesize(int[] durations, bool[] level, string filename, int Frequency = 1100, int Amplitude = 500)
+        {
+            double A = ((Amplitude * (System.Math.Pow(2, 15))) / 1000) - 1;
+            double DeltaFT = 2 * Math.PI * Frequency / 44100.0;
+
+            int Duration = durations.Sum();
+            int Samples = 441 * Duration / 10;
+            int Bytes = Samples * 4;
+            int[] Hdr = { 0X46464952, 36 + Bytes, 0X45564157, 0X20746D66, 16, 0X20001, 44100, 176400, 0X100004, 0X61746164, Bytes };
+
+            using (BinaryWriter BW = new BinaryWriter(File.Open(filename, FileMode.Create)))
+            {
+                for (int I = 0; I < Hdr.Length; I++)
+                {
+                    BW.Write(Hdr[I]);
+                }
+
+
+                for (int i = 0; i < durations.Length; i++)
+                {
+                    int samples = 441 * durations[i] / 10;
+                    for (int T = 0; T < samples; T++)
+                    {
+                        short Sample = level[i] ? System.Convert.ToInt16(A * Math.Sin(DeltaFT * T)) : System.Convert.ToInt16(0);
+                        BW.Write(Sample);
+                        BW.Write(Sample);
+                    }
+                }               
+
+            }
+
+            return 0;
+        }
+
+    }
+}
